55527ee208f3990fdd89b7c89ba941cc3e93923c
Brian Anderson <briananderson@MacBook-Pro-2.local>
2021-11-09 14:23:43 -0700

    well that rabbithole was fun

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser")
 
 // Import JSON files
 const projects = require("./projects.json");
@@ -12,6 +13,7 @@ const app = express();
 
 // set up middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //home route for testing our app
 app.get("/", (req, res) => {
@@ -29,6 +31,44 @@ app.get("/about", (req, res) => {
   // send projects via JSON
   res.json(about);
 });
+
+
+// // create route - post
+// app.post("/projects", (req, res) => {
+//     // push req.body into array
+//     console.log("Request Body:", req)
+//     projects.push(req.body)
+//     // return all the projects
+//     res.json(projects)
+// })
+
+// // update route - put - 
+// app.put("/projects/:id", (req, res) => {
+//     // grab the id
+//     const id = req.params.id
+//     // update the array
+//     projects[id] = req.body
+//     // return all projects
+//     res.json(projects)
+// })
+
+// // destroy route
+// app.delete("/projects/:id", (req, res) => {
+//     // grab the id
+//     const id = req.params.id
+//     // splice/remove the item from the array
+//     projects.splice(id, 1)
+//     // return all projects
+//     res.json(projects)
+// })
+
+// // show route
+// app.get("/projects/:id", (req, res) => {
+//     // grab the id from params
+//     const id = req.params.id
+//     // send back specific project
+//     res.json(projects[id])
+// })
 
 //declare a variable for our port number
 const PORT = process.env.PORT || 4000;
